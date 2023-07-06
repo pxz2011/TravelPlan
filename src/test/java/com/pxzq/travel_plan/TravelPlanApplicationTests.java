05c6354a8c12cae6ce5f8b5f812a1ae83a04a9d9
pxz2011 <113100518+pxz2011@users.noreply.github.com>
2023-07-06 13:15:40 +0800

    2023-7-6 13:15 master

---
+++ src/test/java/com/pxzq/travel_plan/TravelPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pxzq.travel_plan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelPlanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
