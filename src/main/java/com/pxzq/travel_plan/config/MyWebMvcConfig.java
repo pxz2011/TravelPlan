ceb716bd3b68520893e6886268f031be1c05f733
pxz2011 <113100518+pxz2011@users.noreply.github.com>
2023-07-28 08:48:29 +0800

    master

--- src/main/java/com/pxzq/travel_plan/config/MyWebMvcConfig.java
+++ src/main/java/com/pxzq/travel_plan/config/MyWebMvcConfig.java
@@ -14,6 +14,7 @@ import java.util.concurrent.ThreadPoolExecutor;
 
 @Configuration
 public class MyWebMvcConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
