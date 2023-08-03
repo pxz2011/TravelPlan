4a7a6a2bbee83b13133715479ca811010586132a
pxz2011 <113100518+pxz2011@users.noreply.github.com>
2023-08-03 09:11:34 +0800

    master

--- src/main/java/com/pxzq/travel_plan/TravelPlanApplication.java
+++ src/main/java/com/pxzq/travel_plan/TravelPlanApplication.java
@@ -4,12 +4,19 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @Slf4j
 @SpringBootApplication
 //启动程序
 public class TravelPlanApplication {
-
+    /**
+     * Main
+     *
+     * @param args 参数
+     */
     public static void main(String[] args) {
+        log.info(Arrays.toString(args));
         SpringApplication.run(TravelPlanApplication.class, args);
         log.info("项目启动成功!...");
     }
